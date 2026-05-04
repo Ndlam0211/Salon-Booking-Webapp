7a5554f8a5ed7989f1490afb46a02be539e142b7
Nguyen Dinh Lam <nguyendinhlam0211@gmail.com>
2026-05-04 16:43:28 +0700

    Add Auditable and Salon entity classes with JPA annotations for salon-service

---
+++ salon-service/src/main/java/com/lamnd/entity/Salon.java
@@ -0,0 +1,46 @@
+package com.lamnd.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.*;
+
+import java.time.LocalTime;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "salons")
+public class Salon extends Auditable {
+    @Column(nullable = false)
+    private String name;
+
+    @ElementCollection
+    private List<String> images;
+
+    @Column(nullable = false)
+    private String address;
+
+    @Column(nullable = false)
+    private String phoneNumber;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String city;
+
+    @Column(nullable = false)
+    private Long ownerId;
+
+    @Column(nullable = false)
+    private LocalTime openingTime;
+
+    @Column(nullable = false)
+    private LocalTime closingTime;
+}
