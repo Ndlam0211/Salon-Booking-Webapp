dd9e090ef80ec26d21a25664d34c6118cc98daf9
Nguyen Dinh Lam <nguyendinhlam0211@gmail.com>
2026-05-04 22:58:36 +0700

    Refactor getSalonByOwnerId method to remove unnecessary path variable

--- salon-service/src/main/java/com/lamnd/dto/request/SalonCreateRequest.java
+++ salon-service/src/main/java/com/lamnd/dto/request/SalonCreateRequest.java
@@ -1,12 +1,17 @@
 package com.lamnd.dto.request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
 import java.util.List;
 
 public record SalonCreateRequest(
+    @NotBlank(message = "Name is required")
     String name,
     List<String> images,
     String address,
     String phoneNumber,
+    @Email
     String email,
     String city,
     String openingTime,
