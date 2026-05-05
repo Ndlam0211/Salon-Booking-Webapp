7a3dad6851974db05b5658107df64b36e3c0c3b2
Nguyen Dinh Lam <nguyendinhlam0211@gmail.com>
2026-05-06 01:00:44 +0700

    Add ServiceOffering repository and implement ServiceOfferingService interface

--- service-offering/src/main/java/com/lamnd/service/impl/ServiceOfferingServiceImpl.java
+++ service-offering/src/main/java/com/lamnd/service/impl/ServiceOfferingServiceImpl.java
@@ -5,13 +5,19 @@ import com.lamnd.dto.SalonDTO;
 import com.lamnd.dto.request.ServiceOfferingCreateRequest;
 import com.lamnd.dto.request.ServiceOfferingUpdateRequest;
 import com.lamnd.dto.response.ServiceOfferingResponse;
+import com.lamnd.repository.ServiceOfferingRepo;
 import com.lamnd.service.ServiceOfferingService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 @Service
+@RequiredArgsConstructor
 public class ServiceOfferingServiceImpl implements ServiceOfferingService {
+
+    private final ServiceOfferingRepo serviceOfferingRepo;
+
     @Override
     public ServiceOfferingResponse createServiceOffering(ServiceOfferingCreateRequest createRequest, SalonDTO salon, CategoryDTO category) {
         return null;
