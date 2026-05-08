7f0054ae93d0f09d0d02c6f7b635d9a3d8f1e71e
Nguyen Dinh Lam <nguyendinhlam0211@gmail.com>
2026-05-08 21:46:43 +0700

    Add BookingRepo interface for booking service

---
+++ booking-service/src/main/java/com/lamnd/repository/BookingRepo.java
@@ -0,0 +1,9 @@
+package com.lamnd.repository;
+
+import com.lamnd.entity.Booking;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookingRepo extends JpaRepository<Booking, Long> {
+}
