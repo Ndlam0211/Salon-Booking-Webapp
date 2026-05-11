b2898494e7a9ad4ab483b60f54b3b4b4de8a2cd5
Nguyen Dinh Lam <nguyendinhlam0211@gmail.com>
2026-05-11 12:02:26 +0700

    Add ApiResponse, BaseController, BaseMapper, and PaymentMapper for standardized API responses and mapping

---
+++ payment-service/src/main/java/com/lamnd/mapper/PaymentMapper.java
@@ -0,0 +1,8 @@
+package com.lamnd.mapper;
+
+import com.lamnd.common.BaseMapper;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface PaymentMapper extends BaseMapper {
+}
