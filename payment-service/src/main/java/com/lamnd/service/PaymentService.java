f765ce81fea4f46ad4345547027fe371fef3fd74
Nguyen Dinh Lam <nguyendinhlam0211@gmail.com>
2026-05-11 12:01:02 +0700

    Refactor application configuration to YAML format; add PaymentService interface and implementation; update dependencies in pom.xml

---
+++ payment-service/src/main/java/com/lamnd/service/PaymentService.java
@@ -0,0 +1,4 @@
+package com.lamnd.service;
+
+public interface PaymentService {
+}
---
+++ payment-service/src/main/java/com/lamnd/service/impl/PaymentServiceImpl.java
@@ -0,0 +1,8 @@
+package com.lamnd.service.impl;
+
+import com.lamnd.service.PaymentService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PaymentServiceImpl implements PaymentService {
+}
