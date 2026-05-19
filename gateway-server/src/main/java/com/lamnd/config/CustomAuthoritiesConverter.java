26565a6c8a59b3cebff20ff3c8cdf37770ffe865
Nguyen Dinh Lam <nguyendinhlam0211@gmail.com>
2026-05-19 23:35:37 +0700

    Enhance CustomAuthoritiesConverter to aggregate roles from both realm and resource access claims

--- gateway-server/src/main/java/com/lamnd/config/CustomAuthoritiesConverter.java
+++ gateway-server/src/main/java/com/lamnd/config/CustomAuthoritiesConverter.java
@@ -5,31 +5,47 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.jwt.Jwt;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 public class CustomAuthoritiesConverter implements Converter<Jwt, Collection<GrantedAuthority>> {
     private final String ROLE_PREFIX = "ROLE_";
 
     @Override
     public Collection<GrantedAuthority> convert(Jwt source) {
+        Collection<GrantedAuthority> authorities = new ArrayList<>();
+
         String REALM_ACCESS = "realm_access";
         String RESOURCE_ACCESS = "resource_access";
+
         Map<String, Object> realmAccessMap = source.getClaimAsMap(REALM_ACCESS);
 
         if (realmAccessMap != null && realmAccessMap.containsKey("roles")) {
             Object roles = realmAccessMap.get("roles");
 
             if (roles instanceof List<?> stringRoles) {
-                return ((List<?>) stringRoles)
-                        .stream()
-                                .map(role -> new SimpleGrantedAuthority(String.format("%s%s", ROLE_PREFIX, role)))
-                                .collect(Collectors.toList());
+                 stringRoles.forEach(role -> authorities.add(
+                         new SimpleGrantedAuthority(String.format("%s%s", ROLE_PREFIX, role))));
             }
         }
 
-        return List.of();
+        Map<String, Object> resourceAccessMap = source.getClaimAsMap(RESOURCE_ACCESS);
+
+        if (resourceAccessMap != null) {
+            resourceAccessMap.forEach((client, clientDetails) -> {
+                Map<String, Object> clientRoles = (Map<String, Object>) clientDetails;
+
+                if (clientRoles.containsKey("roles")) {
+                    List<String> roles = (List<String>) clientRoles.get("roles");
+                    authorities.addAll(roles.stream()
+                            .map(role -> new SimpleGrantedAuthority(String.format("%s%s", ROLE_PREFIX, role)))
+                            .toList());
+                }
+            });
+        }
+
+        return authorities;
     }
 }
